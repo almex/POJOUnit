a6200b5c14a58f8064a9e17cac4fa5357a98dca3
Alexis SOUMAGNE <alexis.soumagne@gmail.com>
2016-01-23 18:08:45 +0100

    Improved code coverage

--- src/test/java/com/github/almex/pojounit/AbstractPojoTestTest.java
+++ src/test/java/com/github/almex/pojounit/AbstractPojoTestTest.java
@@ -4,6 +4,8 @@ import com.github.almex.pojounit.model.Person;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.PrivilegedActionException;
+
 /**
  * @author Almex
  * @since 1.2
@@ -37,12 +39,26 @@ public class AbstractPojoTestTest {
         Assert.assertEquals(expected, actual);
     }
 
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCallCloneWCloneNotSupportedException() throws Exception {
+        AbstractPojoTest.callClone(new PersonWithoutClone());
+    }
+
     @Test
     public void testCallCloneFromSuper() throws Exception {
         SubPerson expected = new SubPerson();
         Object actual = AbstractPojoTest.callClone(expected);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    private class PersonWithoutClone implements Cloneable {
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
     }
 
     private class SubPerson extends Person {}
